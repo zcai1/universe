065f08e6a76816a08f387a77d2a8ccf609a2f14d
Werner Dietl <wdietl@gmail.com>
2015-04-10 06:02:30 +0000

    Try to make scripts work in Vagrant.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,4 @@
-export ROOT=/home/wmdietl/research/eclipse-workspaces/types/all-projects/
-export JAVA_HOME=/home/software/openjdk8/current/jdk1.8.0
+export ROOT=$MYDIR/../..
 
 export JAVAC=$ROOT/jsr308-langtools/dist/bin/javac
 export CHECKERFRAMEWORK=$ROOT/checker-framework
@@ -9,9 +8,10 @@ export PATH=$JAVA_HOME/bin:$PATH
 export JAVA=$JAVA_HOME/bin/java
 export CHINF=$ROOT/checker-framework-inference
 
-export SCALA_HOME=/home/software/scala/scala/current/
-export SCALA=$SCALA_HOME/bin/scala
-export SCALAC=$SCALA_HOME/bin/fsc
+# export SCALA_HOME=/home/software/scala/scala/current/
+# export SCALA=$SCALA_HOME/bin/scala
+# export SCALAC=$SCALA_HOME/bin/fsc
+export SCALAC=fsc
 export JAVACMD=$JAVA
 
 export CFCLASSPATH=$ROOT/checker-framework-inference/bin:$ROOT/universe/bin:$CHINF/bin:$ROOT/jsr308-langtools/build/classes:$CHECKERFRAMEWORK/javacutil/build/:$CHECKERFRAMEWORK/dataflow/build/:$CHECKERFRAMEWORK/framework/build/:$CHECKERFRAMEWORK/stubparser/build/:$CHECKERFRAMEWORK/checker/build/:$CHECKERFRAMEWORK/framework/tests/junit.jar:$CHECKERFRAMEWORK/checker/dist/checker.jar:$JAVA_HOME/lib/tools.jar:$JAVA_HOME/jre/lib/rt.jar:$SCALA_HOME/lib/scala-library.jar:$SCALA_HOME/lib/scala-compiler.jar:$ROOT/plume-lib/java/plume.jar:$ROOT/annotation-tools/annotation-file-utilities/bin:$ROOT/annotation-tools/annotation-file-utilities/annotation-file-utilities.jar:$ROOT/checker-framework-inference/tests:.
