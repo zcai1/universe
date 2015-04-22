4b666c8a451d0fe5143f25ed9866292d0658a9c4
Nahid Juma <ndawood@uwaterloo.ca>
2015-04-22 19:14:32 +0000

    Changed the compileGUT.sh to use -source target 7

--- scripts/compileGUT.sh
+++ scripts/compileGUT.sh
@@ -3,6 +3,6 @@
 export MYDIR=`dirname $0`
 . $MYDIR/setup.sh
 
-$JAVAC -d $MYDIR/../bin/ `find $MYDIR/../src/ -name "*.java"`
+$JAVAC -source 7 -target 7 -d $MYDIR/../bin/ `find $MYDIR/../src/ -name "*.java"`
 $SCALAC -d $MYDIR/../bin/ `find $MYDIR/../src/ -name "*.scala"` `find $MYDIR/../src/ -name "*.java"` 
 
