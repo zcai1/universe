17b9f4de18ff241fecfcb9836f247f7436cdac31
Mier Ta <m2ta@uwaterloo.ca>
2016-06-14 19:43:58 -0400

    Update Member.java

--- tests/GUTI/Member.java
+++ tests/GUTI/Member.java
@@ -2,11 +2,24 @@ import GUT.qual.Self;
 import GUT.qual.*;
 import java.util.List;
 class E {
-	List<String> le;
+	// Type parameter expects @Peer + @Peer Object = @Peer Object
+	List<Object> le;// If only actual underlying type is the subtype of the declared underlying type, then the qualifier is useless?!
 }
 public class Member {
      
     E e = new E();
-    List<String> l = e.le;
+    //:: fixable-error: (assignment.type.incompatible)
+    @Rep List<@Rep Object> l = e.le;
+    
+    //@Peer List<@Peer Object> l2 = e.le;
+
+    /*
+    @Peer List<@Peer String> ln;
+    @Peer List<@Any String> ln;
+    @Peer List<@Rep String> ln2 = ln;*/
+    
+    /*
+    @Peer List<@Peer Object> ln;
+    @Peer List<@Rep Object> ln2 = ln;*/
 
 }
