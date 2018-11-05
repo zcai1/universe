149acd9d5ec5f8282b56ae0b04e8886353120e34
Jeff Luo <j36luo@uwaterloo.ca>
2018-11-05 13:39:23 -0500

    adapt GUTViewpointAdapter to use AbstractVPAdaptor

--- src/universe/GUTViewpointAdapter.java
+++ src/universe/GUTViewpointAdapter.java
@@ -1,14 +1,5 @@
 package universe;
 
-
-import org.checkerframework.framework.type.AnnotatedTypeFactory;
-import org.checkerframework.framework.type.AnnotatedTypeMirror;
-import org.checkerframework.framework.util.FrameworkViewpointAdapter;
-import org.checkerframework.javacutil.AnnotationUtils;
-import org.checkerframework.javacutil.ErrorReporter;
-
-import javax.lang.model.element.AnnotationMirror;
-
 import static universe.GUTChecker.ANY;
 import static universe.GUTChecker.BOTTOM;
 import static universe.GUTChecker.LOST;
@@ -16,37 +7,48 @@ import static universe.GUTChecker.PEER;
 import static universe.GUTChecker.REP;
 import static universe.GUTChecker.SELF;
 
-public class GUTViewpointAdapter extends FrameworkViewpointAdapter {
+import javax.lang.model.element.AnnotationMirror;
+
+import org.checkerframework.framework.type.AbstractViewpointAdapter;
+import org.checkerframework.framework.type.AnnotatedTypeFactory;
+import org.checkerframework.framework.type.AnnotatedTypeMirror;
+import org.checkerframework.javacutil.AnnotationUtils;
+
+public class GUTViewpointAdapter extends AbstractViewpointAdapter {
+
+    public GUTViewpointAdapter(AnnotatedTypeFactory atypeFactory) {
+        super(atypeFactory);
+    }
 
     @Override
-    protected AnnotationMirror getModifier(AnnotatedTypeMirror atm, AnnotatedTypeFactory f) {
+    protected AnnotationMirror extractAnnotationMirror(AnnotatedTypeMirror atm) {
         assert atm != null;
         return atm.getAnnotationInHierarchy(ANY);
     }
 
     @Override
-    protected AnnotationMirror combineModifierWithModifier(AnnotationMirror recvModifier, AnnotationMirror declModifier,
-            AnnotatedTypeFactory f) {
-        assert recvModifier != null;
-        assert declModifier != null;
+    protected AnnotationMirror combineAnnotationWithAnnotation(AnnotationMirror receiverAnnotation,
+            AnnotationMirror declaredAnnotation) {
+        assert receiverAnnotation != null;
+        assert declaredAnnotation != null;
 
-        if (AnnotationUtils.areSame(recvModifier, SELF)) {
-            return declModifier;
-        } else if (AnnotationUtils.areSame(declModifier, BOTTOM)) {
+        if (AnnotationUtils.areSame(receiverAnnotation, SELF)) {
+            return declaredAnnotation;
+        } else if (AnnotationUtils.areSame(declaredAnnotation, BOTTOM)) {
             return BOTTOM;
-        } else if (AnnotationUtils.areSame(declModifier, ANY)) {
+        } else if (AnnotationUtils.areSame(declaredAnnotation, ANY)) {
             return ANY;
-        } else if (AnnotationUtils.areSame(recvModifier, BOTTOM)) {
+        } else if (AnnotationUtils.areSame(receiverAnnotation, BOTTOM)) {
             // If receiver is bottom, has no ownership information. Any member
             // of it from the viewpoint of self is any, except when declared
             // type is bottom.
             return ANY;
-        }  else if (AnnotationUtils.areSame(declModifier, LOST)) {
+        } else if (AnnotationUtils.areSame(declaredAnnotation, LOST)) {
             return LOST;
-        }  else if (AnnotationUtils.areSame(declModifier, PEER)) {
-            if (AnnotationUtils.areSame(recvModifier, PEER)) {
+        } else if (AnnotationUtils.areSame(declaredAnnotation, PEER)) {
+            if (AnnotationUtils.areSame(receiverAnnotation, PEER)) {
                 return PEER;
-            } else if (AnnotationUtils.areSame(recvModifier, REP)) {
+            } else if (AnnotationUtils.areSame(receiverAnnotation, REP)) {
                 return REP;
             }
         }
