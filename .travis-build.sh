2dd3d5e5e873459ae7edeae22355a9c082f6db12
Baorui Zhou <b42zhou@uwaterloo.ca>
2019-04-18 18:15:31 -0400

    remove redundant steps in build script

--- .travis-build.sh
+++ .travis-build.sh
@@ -14,17 +14,6 @@ export REPO_SITE=baoruiz
 
 echo "------ Downloading everthing from REPO_SITE: $REPO_SITE ------"
 
-if [ ! -d $JSR308/annotation-tools ] ; then
-    (cd $JSR308 && git clone https://github.com/typetools/annotation-tools.git)
-    (cd $JSR308/annotation-tools && git checkout 643077d70129192e52926b3df8838ae15a0fcd4f)
-fi
-
-# Clone stubparser
-if [ -d $JSR308/stubparser ] ; then
-    (cd $JSR308/stubparser && git pull)
-else
-    (cd $JSR308 && git clone --depth 1 https://github.com/topnessman/stubparser.git)
-fi
 # Clone checker-framework
 if [ -d $JSR308/checker-framework ] ; then
     (cd $JSR308/checker-framework && git checkout pull-pico-changes && git pull)
@@ -39,21 +28,11 @@ else
     (cd $JSR308 && git clone -b pull-pico-changes --depth 1 https://github.com/"$REPO_SITE"/checker-framework-inference.git)
 fi
 
-# Download jsr308-langtools replacement for javac.jar that fixes some bugs
-if [ ! -d $JSR308/jsr308-langtools ] ; then
-  (cd .. && wget -q https://checkerframework.org/jsr308/jsr308-langtools-2.4.0.zip)
-  (cd .. && unzip -q jsr308-langtools-2.4.0.zip)
-  (cd .. && mv jsr308-langtools-2.4.0 jsr308-langtools)
-fi
-
-# Build annotation-tools
-(cd $JSR308/annotation-tools && ./.travis-build-without-test.sh)
-# Build stubparser
-(cd $JSR308/stubparser/ && mvn package -Dmaven.test.skip=true)
 # Build checker-framework, with downloaded jdk
-(cd $JSR308/checker-framework && ./gradlew assemble)
+(cd $JSR308/checker-framework && ./.travis-build-without-test.sh downloadjdk)
+
 # Build checker-framework-inference
-(cd $JSR308/checker-framework-inference && ./gradlew dist) # This step needs to be manually in $CFI executed due to path problems
+(cd $JSR308/checker-framework-inference && ./gradlew dist)
 
 # Build GUT
 (cd $JSR308/universe && ./gradlew build)
